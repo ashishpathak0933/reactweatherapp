382d61b3c515e27fe8aba464e0ab1f387b00282d
Ashish kumar pathak <ashishpathak0933@gmail.com>
2023-09-17 22:33:55 +0200

    littlechange

--- src/temp.js
+++ src/temp.js
@@ -3,16 +3,50 @@ import "./style.css"
 const Temp = () => {
   return (
     <div>
-      <div className='Wrap'>
+      <div className='wrap'>
       <div className='search'>
           <input type="search" 
-           placeholder='Enter the city name'
+           placeholder='search...'
            autoFocus
            id = "search"
            className='searchTerm'/>
+
+           <button className='searchButton' type='button'>
+        Search</button>
       </div>
-        <button className='searchButton' type='button'></button>
       </div>
+
+      <article className='widget'>
+        <div className='weatherIcon'>
+         <i className={"wi wi-day-sunny"}></i>
+        </div>
+
+        <div className='weatherInfo'>
+        <div className='temperature'>
+        <span>25.5</span>
+        </div>
+         
+        <div className='description'>
+        <div className='weatherCondition'>sunny</div>
+           <div className='place'> Ambala, India </div>
+        </div>
+        </div>
+
+        <div className='date'>{new Date().toLocaleString()}</div>
+          {/* ummmm 4 sections jo divdie kiye the 
+         */}
+         <div className='extra-temp'>
+           <div className='temp-info-minmax'>
+           <div className='two-sided-section'>
+           <p> <i className='{} wi wi-sunset'></i></p>
+            <p className='extra-info-leftside'> 19:19 PM  <br /> Sunset</p>
+           </div>
+           
+           
+           </div>
+         </div>
+        
+      </article>
     </div>
   )
 }
