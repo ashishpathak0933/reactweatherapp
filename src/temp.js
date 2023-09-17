a298c342b55011b2e0cc4c6f5092e83c10452064
Ashish kumar pathak <ashishpathak0933@gmail.com>
2023-09-17 23:54:47 +0200

    updates

--- src/temp.js
+++ src/temp.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./style.css"
 const Temp = () => {
+
+const [ searchValue , setSearchValue] = useState("Chemnitz") ;
+
+const getWeatherInfo = async()=> {
+ try{
+  let url =  
+  `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=177f01a2d62e1d420e2bdfdb235f75f4` 
+    const res = await fetch(url);
+   const data = await res.json();
+
+   console.log(data)
+
+ }
+
+catch (error){
+
+console.log(error)
+ }
+
+} ; 
+  
+
+
+
+ useEffect(()=> {
+  getWeatherInfo();
+ } , []);
   return (
     <div>
       <div className='wrap'>
@@ -9,13 +36,20 @@ const Temp = () => {
            placeholder='search...'
            autoFocus
            id = "search"
-           className='searchTerm'/>
+           className='searchTerm'
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+           />
 
-           <button className='searchButton' type='button'>
+           <button className='searchButton' type='button'onClick={getWeatherInfo} >
+
+
         Search</button>
       </div>
       </div>
-
+      
+      {/* nich wala area
+         */}
       <article className='widget'>
         <div className='weatherIcon'>
          <i className={"wi wi-day-sunny"}></i>
@@ -33,18 +67,54 @@ const Temp = () => {
         </div>
 
         <div className='date'>{new Date().toLocaleString()}</div>
+
           {/* ummmm 4 sections jo divdie kiye the 
          */}
-         <div className='extra-temp'>
-           <div className='temp-info-minmax'>
-           <div className='two-sided-section'>
-           <p> <i className='{} wi wi-sunset'></i></p>
-            <p className='extra-info-leftside'> 19:19 PM  <br /> Sunset</p>
-           </div>
-           
-           
-           </div>
-         </div>
+         <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                5:15 PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 <br />
+                Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                 19 :19 <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
         
       </article>
     </div>
